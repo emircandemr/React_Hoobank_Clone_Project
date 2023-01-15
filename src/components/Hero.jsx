bcb94fb11a362063fbd1c6b4177980a916a61eb3
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-01-15 19:47:12 +0300

    🚀 updated : hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import styles from '../style'
+import { discount,robot } from '../assets'
+import GetStarted from './GetStarted'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='home' className={` flex md:flex-row flex-col ${styles.paddingY} `} >
+      <div className={`flex-1 ${styles.flexStart} flex-col sm:px-16 px-6`} >
+        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-lg mb-2'>
+          <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
+          <p className={`${styles.paragraph} ml-2`}>
+          <span className='text-white'>20% </span>
+           Discount For {" "}
+          <span className='text-white'> 1 Month </span>
+           Account
+          </p>
+        </div>
+        <div className='flex flex-row justify-between items-center w-full' >
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] text-white '>
+            The Next <br className='sm:block hidden'/> {" "}
+            <span className='text-gradient'>Generation</span>
+          </h1>
+          <div className='ss:flex hidden text-white md:mr-4 mr-0' >
+              <GetStarted/>
+          </div>
+        </div>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] ss:leading-[100px] w-full text-white'>
+          Payment Method.
+        </h1>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav className='w-full flex py-6 justify-between items-center navbar ' >
       <img src={logo} alt='hoobank' className='w-[124px] h-[32px] ' />
 
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1 bg-red-500 '>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1  '>
         {navLinks.map((link,index) => (
           <li key={link.id} 
           className={`font-poppins font-normal cursor-pointer text-[16px] text-white
@@ -21,7 +21,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className='sm:hidden flex flex-1 justify-end bg-blue-500 items-center' >
+      <div className='sm:hidden flex flex-1 justify-end  items-center' >
           <img src={toggle ? close : menu} alt='menu'
           className='w-[20px] h-[20px] object-contain'
           onClick={() => setToggle(!toggle)}
@@ -31,7 +31,7 @@ const Navbar = () => {
           className={`${toggle ? 'flex' : 'hidden' } p-6 bg-black-gradient absolute top-20 right-0 mx-4 
           my-2 min-w-[140px] rounded-[10px] shadow-lg z-10 sidebar
           `} >
-          <ul className='list-none flex flex-col justify-end items-center flex-1 bg-red-500 '>
+          <ul className='list-none flex flex-col justify-end items-center flex-1  '>
             {navLinks.map((link,index) => (
               <li key={link.id} 
               className={`font-poppins font-normal cursor-pointer text-[16px] text-white
