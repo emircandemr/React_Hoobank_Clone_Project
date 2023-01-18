92cffaccc68cbd823c476f821802b6c12751546f
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-01-18 21:57:46 +0300

    ✨ added : billing comp

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {apple,bill,google} from '../assets'
+import styles , {layout} from '../style'
 
 const Billing = () => {
+  
   return (
-    <div>Billing</div>
+    <section id='product' className={layout.sectionReverse} >
+      <div className={layout.sectionImgReverse} >
+        <img src={bill} alt='billing' className='w-[100%] h-[100%] relative z-[5] '  /> 
+      </div>
+      <div className={layout.sectionInfo} >
+        <h2 className={styles.heading2} >Easily control your <br className='sm:block hidden' /> billing a invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 `} >
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6' >
+          <img src={apple} alt="play" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer '  />
+          <img src={google} alt="google" className='w-[128px] h-[42px] object-contain cursor-pointer '  />
+        </div>
+      </div>
+    </section>
   )
 }
 
