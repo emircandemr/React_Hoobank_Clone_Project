6c38b01279c060424e8d2c74eeb6ab830d4424d4
emircandemr <72731296+emircandemr@users.noreply.github.com>
2023-01-20 21:03:39 +0300

    ✨ added : carddeal components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './style'
 import {Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonial,Clients,CTA,Footer} from './components/'
 
 const App = () => {
+
+  const [scrolling,setIsScrolling] = useState(false)
+
+  window.onscroll = () => {
+    setIsScrolling(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  }
+
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}> 
-        <div className={`${styles.boxWidth}`}>
+        <div className={`w-full px-16 fixed top-0 z-50 ${scrolling ? 'bg-gray-gradient' : '' } `}>
           <Navbar />
         </div>
       </div>
 
       <div className={`${styles.flexStart}   bg-primary`}>
-        <div className={`${styles.boxWidth}`}>
+        <div className={`${styles.boxWidth} mt-16 sm:mt-6 `}>
           <Hero></Hero>
         </div>
       </div>
